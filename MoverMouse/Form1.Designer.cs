76579a45278adfbcdab83205420c39f26b90fb6a
Ezequiel López <zql9000@users.noreply.github.com>
2018-05-09 13:57:06 -0300

    Primer versión

---
+++ MoverMouse/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MoverMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrecuencia = new System.Windows.Forms.NumericUpDown();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.txtFrecuencia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = ".";
+            this.notifyIcon1.BalloonTipTitle = "Mover puntero del mouse";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frecuencia de movimiento:";
+            // 
+            // txtFrecuencia
+            // 
+            this.txtFrecuencia.Location = new System.Drawing.Point(164, 47);
+            this.txtFrecuencia.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.txtFrecuencia.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtFrecuencia.Name = "txtFrecuencia";
+            this.txtFrecuencia.Size = new System.Drawing.Size(120, 20);
+            this.txtFrecuencia.TabIndex = 1;
+            this.txtFrecuencia.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.txtFrecuencia.ValueChanged += new System.EventHandler(this.txtFrecuencia_ValueChanged);
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.Location = new System.Drawing.Point(209, 96);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(75, 23);
+            this.btnAccion.TabIndex = 2;
+            this.btnAccion.Text = "Detener";
+            this.btnAccion.UseVisualStyleBackColor = true;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 171);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.txtFrecuencia);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mover el puntero del mouse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.txtFrecuencia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown txtFrecuencia;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MoverMouse/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MoverMouse
+{
+    public partial class Form1 : Form
+    {
+        private enum eAcciones
+        {
+            Detener = 0,
+            Iniciar = 1
+        }
+        eAcciones Accion;
+        private enum eSentidos
+        {
+            Derecha = 0,
+            Izquierda = 1
+        }
+        eSentidos Sentido;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(250);
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)txtFrecuencia.Value * 1000;
+            Accion = eAcciones.Detener;
+            btnAccion.Text = eAcciones.Detener.ToString();
+            notifyIcon1.BalloonTipText = string.Format("Se moverá el mouse cada {0} segundos.", txtFrecuencia.Value);
+            this.WindowState = FormWindowState.Minimized;
+            timer1.Start();
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            if (Accion == eAcciones.Detener)
+            {
+                Accion = eAcciones.Iniciar;
+                btnAccion.Text = eAcciones.Iniciar.ToString();
+                notifyIcon1.BalloonTipText = "El mouse no se moverá porque se encuentra detenido.";
+                timer1.Stop();
+            }
+            else
+            {
+                Accion = eAcciones.Detener;
+                btnAccion.Text = eAcciones.Detener.ToString();
+                notifyIcon1.BalloonTipText = string.Format("Se moverá el mouse cada {0} segundos.", timer1.Interval / 1000);
+                timer1.Start();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+        }
+
+        private void txtFrecuencia_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)txtFrecuencia.Value * 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Sentido == eSentidos.Derecha)
+            {
+                Point actual = Cursor.Position;
+                actual.Offset(10, 0);
+                Cursor.Position = actual;
+
+                if (actual.X >= Screen.PrimaryScreen.WorkingArea.Right)
+                {
+                    Sentido = eSentidos.Izquierda;
+                }
+            }
+            else
+            {
+                Point actual = Cursor.Position;
+                actual.Offset(-10, 0);
+                Cursor.Position = actual;
+
+                if (actual.X <= Screen.PrimaryScreen.WorkingArea.Left)
+                {
+                    Sentido = eSentidos.Derecha;
+                }
+            }
+        }
+    }
+}
